baed5850e19aa4452b847bc41186cf440087c70f
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-01-26 20:44:57 -0600

    Wrote out render method and toggle.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+import './components/TodoComponents/Todo.css';
+
+const todoData = [
+	{
+		task      : 'Organize Garage',
+		id        : 1528817077286,
+		completed : false,
+	},
+	{
+		task      : 'Bake Cookies',
+		id        : 1528817084358,
+		completed : false,
+	},
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+
+		this.state = {
+			todoData : todoData,
+			list     : '',
+		};
+	}
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+
+	toggleList = (id) => {
+		this.setState({
+			todoData : this.state.todoData.map((item) => {
+				if (item.id === id) {
+					return {
+						...item,
+						completed : !item.completed,
+					};
+				} else {
+					return item;
+				}
+			}),
+		});
+  };
+  
+  
+	render() {
+		return (
+			<div className='container'>
+				<h2>Welcome to your To-Do App!</h2>
+				<TodoList todos={this.state.todoData} toggleList={this.toggleList} />
+				<TodoForm
+					addItem={this.addItem}
+					clearCompleted={this.clearCompleted}
+					submitTodo={this.submitTodo}
+					handleChanges={this.handleChanges}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;

a93a8a33d6a3ccac26e3b48da249f85dee7d186c
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-01-26 21:11:56 -0600

    Added class to Add button and placeholder text to input.

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function TodoForm(props) {
 	return (
 		<form onSubmit={props.submitTodo}>
-			<input type='text' value={props.list} name='list' onChange={props.handleChanges} />
-			<button>Add</button>
+			<input type='text' value={props.list} name='list' onChange={props.handleChanges} placeholder='Enter task here' />
+			<button className='submit-btn'>Add</button>
 			<button className='clear-btn' onClick={props.clearCompleted}>
 				Clear Completed
 			</button>

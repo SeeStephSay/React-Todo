c978749d32df36c48fadaf1f2ca9efb46f4de3af
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-01-26 20:24:41 -0600

    Completed TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default function TodoForm(props) {
-  return (
-    <>
-      <form onSumbit={props.submitTodo}>
-        
-      </form>
-    </>
-  )
+	return (
+		<form onSumbit={props.submitTodo}>
+			<input type='text' value={props.list} name='list' onChange={props.handleChanges} />
+			<button>Add</button>
+			<button className='clear-btn' onClick={props.clearCompleted}>
+				Clear clearCompleted
+			</button>
+		</form>
+	);
 }

9c78081368ef91aa808f39e513e8da666d7f6c08
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-03-11 21:07:38 -0500

    Added exampleTodos and outlined App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 
+const exampleTodos = [
+	{
+		name      : 'Go to class',
+		id        : 1,
+		completed : false,
+	},
+	{
+		name      : 'Learn Class Components',
+		id        : 2,
+		completed : false,
+	},
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+	// you will need a place to store your state in this component.
+	// design `App` to be the parent component of your application.
+	// this component is going to take care of state, and any change handlers you need to work with your state
+	constructor() {
+		super();
+		this.state = {
+			todos : exampleTodos,
+		};
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<div className='header'>
+					<h2>To-Dos</h2>
+					<TodoForm />
+				</div>
+				<TodoList />
+			</div>
+		);
+	}
 }
 
 export default App;
